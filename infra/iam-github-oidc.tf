07d2f29ebd66797aca9df63c911d7952b2731240
Aosman4395 <ahamed.osman4395@gmail.com>
2026-04-15 08:54:31 +0100

    new

--- infra/iam-github-oidc.tf
+++ infra/iam-github-oidc.tf
@@ -31,7 +31,7 @@ resource "aws_iam_role" "github_actions" {
             "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
           }
           StringLike = {
-            "token.actions.githubusercontent.com:sub" = "repo:*/*:*"
+            "token.actions.githubusercontent.com:sub" = "repo:Aosman4395/ECS-project:ref:refs/heads/main"
           }
         }
       }
